cca1fe5d97e7845e15aa446282797eb4232c6e3a
Sion Kim <uut_713@naver.com>
2023-06-27 20:46:22 +0900

    0627

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { StoreNewList } from './pages/store_newList';
+import { StoreMyList } from './pages/store_myList';
+import { RiderNewList } from './pages/rider_newList';
+import { RiderDeliveryStatus } from './pages/rider_deliveryStatus';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='bg-grayTone-white border-2 border-grayTone-black max-w-screen-md max-h-[844px] h-screen m-auto flex flex-col'>
+        <Routes>
+          <Route path='/store/newlist' element={<StoreNewList />} />
+          <Route path='/store/mylist' element={<StoreMyList />} />
+          <Route path='/rider/delivery' element={<RiderDeliveryStatus />} />
+          <Route path='/rider/newlist' element={<RiderNewList />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/rider_deliveryStatus.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const RiderDeliveryStatus = () => {
+  return <div></div>;
+};
