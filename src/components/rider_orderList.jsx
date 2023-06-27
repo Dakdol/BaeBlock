26d816bac5d28822d9625d2031aa098dbc73c0d2
Sion Kim <uut_713@naver.com>
2023-06-27 20:55:24 +0900

    0627-2

--- src/components/rider_orderList.jsx
+++ src/components/rider_orderList.jsx
@@ -9,11 +9,15 @@ export const RiderOrderList = () => {
       <ul>
         {testDB.customer.map((customer, i) => (
           <li key={i} className='bg-primary-darkYellow my-4 rounded-[10px]'>
-            <div>
-              배달료: {testDB.customer[i].deliveryFee * 2} 배달팁: {testDB.customer[i].deliveryTip}{' '}
-            </div>
-            <div>
-              고객 주소: {testDB.customer[i].address}매장 주소: {testDB.store[i].address}
+            <div className='px-4 py-2'>
+              <div>
+                배달료: {testDB.customer[i].deliveryFee * 2} <br />
+                배달팁: {testDB.customer[i].deliveryTip}{' '}
+              </div>
+              <div>
+                고객 주소: {testDB.customer[i].address} <br />
+                매장 주소: {testDB.store[i].address}
+              </div>
             </div>
           </li>
         ))}
