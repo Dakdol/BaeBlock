da543ddf578c2ed7f796c6714ae2f5fe2c600e60
Sion Kim <uut_713@naver.com>
2023-07-08 01:51:24 +0900

    카카오 지도 API 추가

--- src/components/menuList.jsx
+++ src/components/menuList.jsx
@@ -1,43 +1,35 @@
-import { Link, useParams } from "react-router-dom";
-import pencil from "../images/icon_pencil_.png";
+import { Link, useParams } from 'react-router-dom';
+import pencil from '../images/icon_pencil_.png';
 
-const MenuList = ({
-  menuId,
-  name,
-  caption,
-  price,
-  isRecommend,
-  menuImage,
-  showPencile,
-}) => {
+const MenuList = ({ menuId, name, caption, price, isRecommend, menuImage, showPencile }) => {
   const { storeId } = useParams();
 
   return (
     <Link to={`/customer/viewmenu/${storeId}/${menuId}`}>
-      <div className="bg-white w-[350px] h-[100px] flex justify-between items-center p-4 rounded-2xl border-[1.5px] border-darkGray solid-shadow">
-        <div className="flex flex-col">
-          <div className="flex flex-col">
-            <div className="flex gap-2 items-center">
-              <div className="text-subtitle font-bold">{name}</div>
+      <div className='bg-white w-[350px] h-[100px] flex justify-between items-center p-4 rounded-2xl border-[1.5px] border-darkGray solid-shadow'>
+        <div className='flex flex-col'>
+          <div className='flex flex-col'>
+            <div className='flex gap-2 items-center'>
+              <div className='text-subtitle font-bold'>{name}</div>
               {isRecommend ? (
-                <div className="bg-deepYellow rounded-full px-1 text-white text-caption">
+                <div className='bg-deepYellow rounded-full px-1 text-white text-caption'>
                   사장님 추천
                 </div>
               ) : (
                 <div></div>
               )}
             </div>
-            <div className="text-caption text-darkGray">{caption}</div>
+            <div className='text-caption text-darkGray'>{caption}</div>
           </div>
-          <div className="text-caption text-darkGray mt-1">{price}원</div>
+          <div className='text-caption text-darkGray mt-1'>{price}원</div>
         </div>
 
-        <div className="relative bg-darkGray rounded-lg w-20 h-20 border-4 border-deepYellow">
-          <img src={menuImage} alt="" />
+        <div className='relative bg-darkGray rounded-lg w-20 h-20 border-4 border-deepYellow'>
+          <img src={menuImage} alt='' />
           {showPencile && (
-            <div className="absolute bottom-[-12px] right-[-12px]">
-              <Link to="/store/edit/menu">
-                <img src={pencil} alt="pencil" />
+            <div className='absolute bottom-[-12px] right-[-12px]'>
+              <Link to='/store/edit/menu'>
+                <img src={pencil} alt='pencil' />
               </Link>
             </div>
           )}
