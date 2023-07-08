5e703a83f81e614a90a4b912cfc42838f62eecf1
Sion Kim <uut_713@naver.com>
2023-07-08 16:58:04 +0900

    글씨 정렬 수정

--- src/components/NftBox.jsx
+++ src/components/NftBox.jsx
@@ -3,7 +3,7 @@ const NftBox = (props) => {
     <div
       className={`flex flex-col items-start justify-center ${props.color} ${props.fontColor} w-[360px] h-[180px] rounded-2xl border-[1.5px] border-black basic-shadow`}>
       <div className='font-bold text-headline tracking-tighter ml-4 mb-2'>{props.title}</div>
-      <div className='ml-4'>
+      <div className='text-left ml-4'>
         {props.detail1}
         <br />
         {props.detail2}
