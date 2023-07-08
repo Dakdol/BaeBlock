a8aa7926adb93a908ffa475e8de29ba3d06022db
Sion Kim <uut_713@naver.com>
2023-07-08 21:00:22 +0900

    0708 임시

--- src/pages/customer_regist.jsx
+++ src/pages/customer_regist.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MMLogo from '../images/logo_metaMask.png';
 import Logo from '../images/BBLogoLightGradient.png';
