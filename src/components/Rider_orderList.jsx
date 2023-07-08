68ba77536ed401d620c289944a71e3b98b155676
Dakdol <jin1290ok@gmail.com>
2023-07-08 14:06:57 +0900

    라이더 web3 연결

--- src/components/Rider_orderList.jsx
+++ src/components/Rider_orderList.jsx
@@ -1,16 +1,43 @@
-import user from '../db/user.json';
+import user from "../db/user.json";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export const RiderOrderList = () => {
-  // 리스트 선택 시 고를 수 있는 기능
+  const { account, orderContract, order_c_address } = useContext(AppContext);
+
+  const onClickSetDelivery = async (orderNumber) => {
+    try {
+      await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: account,
+            to: order_c_address,
+            data: orderContract.methods.setDelivery(orderNumber).encodeABI(),
+            gas: "300000",
+          },
+        ],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const RiderPick = () => (
+    <div className="bg-yellow-400 flex flex-col justify-center items-center">
+      <button onClick={() => onClickSetDelivery(0)}>선택</button>
+    </div>
+  );
 
   return (
-    <div className='flex flex-col '>
+    <div className="flex flex-col ">
       {user.customer.map((v, i) => (
         <button
           key={i}
-          className='bg-white w-[350px] mb-3 rounded-lg border-[1.5px] border-darkGray solid-shadow'>
-          <div className='px-4 py-2'>
-            <div className='flex justify-between font-bold'>
+          className="bg-white w-[350px] mb-3 rounded-lg border-[1.5px] border-darkGray solid-shadow"
+        >
+          <div className="px-4 py-2">
+            <div className="flex justify-between font-bold">
               <div>15분</div>
               <div>
                 {(
@@ -20,15 +47,18 @@ export const RiderOrderList = () => {
                 원
               </div>
             </div>
-            <div className='text-caption'>
-              <div className='flex justify-between mb-2'>
+            <div className="text-caption">
+              <div className="flex justify-between mb-2">
                 {user.customer[i].orderList.storeName}
-                <span className='ml-8'>거리</span>
+                <span className="ml-8">거리</span>
               </div>
-              <div className='flex'>고객 위치: {user.customer[i].cus_address}</div>
+              <div className="flex">
+                고객 위치: {user.customer[i].cus_address}
+              </div>
               {/* <div className='flex'>매장 위치: {user.store[i].sto_address}</div> */}
             </div>
           </div>
+          <RiderPick />
         </button>
       ))}
     </div>
--- src/pages/rider_deliveryStatus.jsx
+++ src/pages/rider_deliveryStatus.jsx
@@ -1,33 +1,76 @@
-import React, { useState } from 'react';
-import { RiderSelectedOrder } from '../components/Rider_selectedOrder';
+import React, { useState } from "react";
+import { RiderSelectedOrder } from "../components/Rider_selectedOrder";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export const RiderDeliveryStatus = () => {
   const [finished, setFinished] = useState(false);
+  const { account, orderContract, order_c_address } = useContext(AppContext);
 
-  const onClickFinish = () => {
-    setFinished(!finished);
+  const onClickFinish = async () => {
+    try {
+      await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: account,
+            to: order_c_address,
+            data: orderContract.methods
+              .doneDelivery(0)
+              .encodeABI() /*주문번호*/,
+            gas: "300000",
+          },
+        ],
+      });
+      setFinished(!finished);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickRiderCompleteAndPay = async () => {
+    try {
+      await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: account,
+            to: order_c_address,
+            data: orderContract.methods
+              .orderComplete(0, true)
+              .encodeABI() /* 주문번호,true*/,
+            gas: "100000",
+          },
+        ],
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
-      <div className='flex flex-col justify-center items-center mt-4'>
-        <div className='flex justify-center'>
-          <div className='border-[1px] px-2 rounded-lg max-w-[250px] border-darkGray whitespace-nowrap overflow-ellipsis overflow-hidden'>
+      <div className="flex flex-col justify-center items-center mt-4">
+        <div className="flex justify-center">
+          <div className="border-[1px] px-2 rounded-lg max-w-[250px] border-darkGray whitespace-nowrap overflow-ellipsis overflow-hidden">
             서울시 강남구 언주로 18길 여기는 잘리는 곳
           </div>
         </div>
 
         {finished ? (
-          <div className='flex justify-center items-center z-30'>
-            <div className='flex flex-col justify-between absolute w-72 h-44 py-4 mt-[520px] bg-white border-2 border-black solid-shadow px-4 rounded-2xl text-black'>
-              <div className='flex flex-col gap-2 justify-center items-center'>
-                <div className='font-bold text-headline'>배달을 완료했어요!</div>
-                <div className='text-caption'>고객님께 배달 완료를 알려요!</div>
+          <div className="flex justify-center items-center z-30">
+            <div className="flex flex-col justify-between absolute w-72 h-44 py-4 mt-[520px] bg-white border-2 border-black solid-shadow px-4 rounded-2xl text-black">
+              <div className="flex flex-col gap-2 justify-center items-center">
+                <div className="font-bold text-headline">
+                  배달을 완료했어요!
+                </div>
+                <div className="text-caption">고객님께 배달 완료를 알려요!</div>
               </div>
-              <div className='flex justify-center gap-4'>
+              <div className="flex justify-center gap-4">
                 <button
-                  className='bg-lightYellow w-20 p-2 rounded-xl font-bold border-[1.5px] border-black'
-                  onClick={onClickFinish}>
+                  className="bg-lightYellow w-20 p-2 rounded-xl font-bold border-[1.5px] border-black"
+                  onClick={onClickRiderCompleteAndPay}
+                >
                   완료
                 </button>
               </div>
@@ -35,16 +78,16 @@ export const RiderDeliveryStatus = () => {
           </div>
         ) : null}
 
-        <div className='w-full h-[520px] relative overflow-hidden mt-6'>
+        <div className="w-full h-[520px] relative overflow-hidden mt-6">
           <img
-            className='absolute top-0 left-0 w-full h-full object-cover'
-            src={process.env.PUBLIC_URL + '/images/testMap.jpg'}
-            alt='지도'
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            src={process.env.PUBLIC_URL + "/images/testMap.jpg"}
+            alt="지도"
           />
         </div>
       </div>
 
-      <div className='flex overflow-x-auto pt-5 pb-2 ml-5'>
+      <div className="flex overflow-x-auto pt-5 pb-2 ml-5">
         <RiderSelectedOrder onClickEvent={onClickFinish} />
         <RiderSelectedOrder onClickEvent={onClickFinish} />
         <RiderSelectedOrder onClickEvent={onClickFinish} />
