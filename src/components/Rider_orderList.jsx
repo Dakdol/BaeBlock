95f42a9735c305215092e10c647a3d5577644497
Sion Kim <uut_713@naver.com>
2023-07-10 19:10:49 +0900

    :zap: 라이더 배달 목록 정렬 애니메이션 추가
    정렬 애니메이션이 추가됌

--- src/components/Rider_orderList.jsx
+++ src/components/Rider_orderList.jsx
@@ -1,7 +1,8 @@
 import { useContext, useState } from 'react';
+import FlipMove from 'react-flip-move';
 import { AppContext } from '../App';
 
-export const RiderOrderList = ({ orders, setSelectDelivery, selectDelivery }) => {
+export const RiderOrderList = ({ orders, setSelectDelivery, activeBtn }) => {
   const { account, orderContract, order_c_address } = useContext(AppContext);
   const [selected, setSelected] = useState({});
   const [toggle, setToggle] = useState({ index: null });
@@ -42,7 +43,7 @@ export const RiderOrderList = ({ orders, setSelectDelivery, selectDelivery }) =>
     <div className='flex flex-col '>
       {toggle.index !== null && (
         <div className='flex justify-center items-center'>
-          <div className='flex flex-col justify-between absolute w-72 h-44 py-4 mt-[350px] bg-white border-2 border-black solid-shadow px-4 rounded-2xl text-black'>
+          <div className='flex flex-col justify-between absolute z-10 w-72 h-44 py-4 mt-[350px] bg-white border-2 border-black solid-shadow px-4 rounded-2xl text-black'>
             <div className='flex flex-col gap-2 justify-center items-center'>
               <div className='font-bold text-headline'>주문을 선택할까요?</div>
               <div className='text-caption'>해당 주문을 선택해요!</div>
@@ -63,30 +64,40 @@ export const RiderOrderList = ({ orders, setSelectDelivery, selectDelivery }) =>
         </div>
       )}
 
-      {orders.map((order, i) => {
-        if (selected[i]) return null;
-        return (
-          <button
-            key={i}
-            className='bg-white w-[350px] mb-3 rounded-lg border-[1.5px] border-darkGray solid-shadow hover:border-2 hover:border-black'
-            onClick={() => onClickToggle(i)}>
-            <div className='px-4 py-2'>
-              <div className='flex justify-between font-bold'>
-                <div>15분</div>
-                <div>{(order.deliveryFee * 2 + order.deliveryTip).toLocaleString()}원</div>
-              </div>
-              <div className='text-caption'>
-                <div className='flex justify-between mb-2'>
-                  {order.storeName}
-                  <span className='ml-8'>{order.distance} km</span>
+      <FlipMove className='flex flex-col justify-center' duration={400} easing='ease-in-out'>
+        {orders.map((order, i) => {
+          if (selected[i]) return null;
+          return (
+            <button
+              key={order.id}
+              className='bg-white w-[350px] mb-3 rounded-lg border-[1.5px] border-darkGray solid-shadow hover:border-2 hover:border-black'
+              onClick={() => onClickToggle(i)}>
+              <div className='px-4 py-2'>
+                <div className='flex justify-between'>
+                  <div className='font-bold'>15분</div>
+                  <div
+                    className={` ${activeBtn === 'fee' ? 'text-purple font-bold' : 'text-black'} `}>
+                    {(order.deliveryFee * 2 + order.deliveryTip).toLocaleString()}원
+                  </div>
                 </div>
-                <div className='flex'>고객 위치: {order.cus_address}</div>
-                {/* <div className='flex'>매장 위치: {order.sto_address}</div> */}
+                <div className='text-caption'>
+                  <div className='flex justify-between mb-2'>
+                    {order.storeName}
+                    <span
+                      className={` ${
+                        activeBtn === 'dist' ? 'text-purple font-bold' : 'text-black'
+                      } ml-8`}>
+                      {order.distance} km
+                    </span>
+                  </div>
+                  <div className='flex'>고객 위치: {order.cus_address}</div>
+                  {/* <div className='flex'>매장 위치: {order.sto_address}</div> */}
+                </div>
               </div>
-            </div>
-          </button>
-        );
-      })}
+            </button>
+          );
+        })}
+      </FlipMove>
     </div>
   );
 };
